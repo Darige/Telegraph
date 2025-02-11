4c47d72893ce98b7a721e141cc42b57706e6bc51
Aung Myo Htet <106561329+Darige@users.noreply.github.com>
2025-02-11 12:57:51 +0000

    Add files via upload

--- myweatherapp-tech-test-main/src/main/java/com/weatherapp/myweatherapp/model/CityInfo.java
+++ myweatherapp-tech-test-main/src/main/java/com/weatherapp/myweatherapp/model/CityInfo.java
@@ -3,29 +3,69 @@ package com.weatherapp.myweatherapp.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
+// Added getters and setters method in this change
+
 public class CityInfo {
 
   @JsonProperty("address")
   String address;
+  
+  public String getAddress() {
+	  return address;
+  }
+  
+  public void setAddress(String city) {
+	  address = city;
+	  return;
+  }
+  
+  
 
   @JsonProperty("description")
   String description;
 
   @JsonProperty("currentConditions")
   CurrentConditions currentConditions;
+  
+  public CurrentConditions getCurrentConditions() {
+	  return currentConditions;
+  }
+  
+  public void setCurrentConditions(CurrentConditions c) {
+	  currentConditions = c;
+	  return;
+  }
 
   @JsonProperty("days")
   List<Days> days;
 
-  static class CurrentConditions {
+  public static class CurrentConditions {
     @JsonProperty("temp")
     String currentTemperature;
 
     @JsonProperty("sunrise")
     String sunrise;
+    
+    public String getSunrise() {
+    	return sunrise;
+    }
+    
+    public void setSunrise(String time) {
+    	sunrise = time;
+    	return;
+    }
 
     @JsonProperty("sunset")
     String sunset;
+    
+    public String getSunset() {
+    	return sunset;
+    }
+    
+    public void setSunset(String time) {
+    	sunset = time;
+    	return;
+    }
 
     @JsonProperty("feelslike")
     String feelslike;
@@ -35,6 +75,15 @@ public class CityInfo {
 
     @JsonProperty("conditions")
     String conditions;
+    
+    public String getConditions() {
+    	return conditions;
+    }
+    
+    public void setConditions(String condition) {
+    	conditions = condition;
+    	return;
+    }
   }
 
   static class Days {
